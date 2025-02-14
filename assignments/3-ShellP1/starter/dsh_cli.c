2e72eb8bf2d830683a2576691d1738d3237a442b
hitanshi17 <125045043+hitanshi17@users.noreply.github.com>
2025-02-13 23:09:26 -0500

    Update dsh_cli.c

--- assignments/3-ShellP1/starter/dsh_cli.c
+++ assignments/3-ShellP1/starter/dsh_cli.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "dshlib.h"
 
 /*
@@ -46,10 +45,39 @@
  */
 int main()
 {
-    char *cmd_buff;
+    char cmd_buff[SH_CMD_MAX];  
     int rc = 0;
-    command_list_t clist;
+    command_list_t clist;       
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    while (1) {
+        printf("%s", SH_PROMPT);  
+
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL) {
+            printf("\n");  
+            break;
+        }
+
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+
+        if (strcmp(cmd_buff, EXIT_CMD) == 0) {
+            exit(EXIT_SUCCESS);
+        }
+
+        rc = build_cmd_list(cmd_buff, &clist);
+        if (rc == OK) {
+            printf(CMD_OK_HEADER, clist.num);
+            for (int i = 0; i < clist.num; i++) {
+                printf("<%d> %s", i + 1, clist.commands[i].exe);
+                if (strlen(clist.commands[i].args) > 0) {
+                    printf(" [%s]", clist.commands[i].args);
+                }
+                printf("\n");
+            }
+        } else if (rc == WARN_NO_CMDS) {
+            printf("%s\n", CMD_WARN_NO_CMD);
+        } else if (rc == ERR_TOO_MANY_COMMANDS) {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX); 
+}
+    }
+    return 0;
 }
